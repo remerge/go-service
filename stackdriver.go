abbf222db5c5e80cf3497258daf19101af10eb62
Martin Karlsch <martin@karlsch.com>
2018-12-17 14:41:45 +0100

    add stackdriver

---
+++ stackdriver.go
@@ -0,0 +1,42 @@
+package service
+
+import (
+	"cloud.google.com/go/profiler"
+	"os"
+)
+
+func (e *Executor) WithStackDriver() *Executor {
+	flags := e.Command.Flags()
+	flags.BoolVar(
+		&e.enableStackdriver,
+		"enable-stackdriver", e.enableStackdriver,
+		"Enable stackdriver",
+	)
+	return e
+}
+
+func (e *Executor) initStackdriver() nil {
+	if !e.enableStackdriver {
+		return
+	}
+	keyPath := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
+	if keyPath == "" {
+		e.log.Warn("could not start stackdriver profiler: env variable GOOGLE_APPLICATION_CREDENTIALS is empty")
+		return
+	}
+
+	if _, err := os.Stat(keyPath); os.IsNotExist(err) {
+		e.log.Warnf("could not start stackdriver profiler: keyfile does not exist %v", keyPath)
+		return
+	}
+
+	e.log.Info("starting stackdriver profiler")
+
+	if err := profiler.Start(profiler.Config{
+		Service:        e.Name,
+		ServiceVersion: CodeVersion,
+		ProjectID:      "stackdriver-profiler-test",
+	}); err != nil {
+		e.log.Warnf("could not start stackdriver profiler: %v", err)
+	}
+}
