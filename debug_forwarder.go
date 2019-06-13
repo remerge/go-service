964ab2f69a6c6cb6eb9017cd66c40c7c902a557c
Martin Karlsch <martin@karlsch.com>
2019-06-13 15:59:26 +0200

    use a single go routine per debug connection and buffer messages

--- debug_forwarder.go
+++ debug_forwarder.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"sync"
 	"sync/atomic"
+	"time"
 
 	"github.com/spf13/cobra"
 
@@ -27,8 +28,10 @@ type DebugForwaderParams struct {
 func registerDebugForwarder(r Registry) {
 	r.Register(func(params *DebugForwaderParams) (*debugForwader, error) {
 		f := &debugForwader{
-			Port: params.Port,
-			log:  params.Log,
+			Port:   params.Port,
+			log:    params.Log,
+			quit:   make(chan bool),
+			exited: make(chan bool),
 		}
 		f.configureFlags(params.Cmd)
 		return f, nil
@@ -50,6 +53,16 @@ type debugForwader struct {
 	connCount uint32
 	connLn    net.Listener
 	log       cue.Logger
+	quit      chan bool
+	exited    chan bool
+}
+
+type debugConn struct {
+	net.Conn
+	o         sync.Once
+	closeWait sync.WaitGroup
+	msgs      chan []byte
+	forwarder *debugForwader
 }
 
 func (f *debugForwader) Init() error {
@@ -64,30 +77,49 @@ func (f *debugForwader) Init() error {
 	f.connLn = ln
 	go func(ln net.Listener) {
 		for {
-			c, err := ln.Accept()
-			if err != nil {
-				f.log.Error(err, "failed to accept debug listener connection, terminate loop")
-				break
+			select {
+			case <-f.quit:
+				close(f.exited)
+				return
+			default:
+				ln.(*net.TCPListener).SetDeadline(time.Now().Add(250 * time.Millisecond))
+				c, err := ln.Accept()
+				if err != nil {
+					if os.IsTimeout(err) {
+						break
+					}
+					f.log.Error(err, "failed to accept debug listener connection, terminate loop")
+					return
+				}
+				f.log.WithFields(cue.Fields{"remote_addr": c.RemoteAddr().String()}).Info("debug connection opened")
+				atomic.AddUint32(&f.connCount, 1)
+				dc := &debugConn{
+					Conn:      c,
+					forwarder: f,
+					msgs:      make(chan []byte, 1024),
+				}
+				dc.closeWait.Add(1)
+				f.conns.Store(c.RemoteAddr().String(), dc)
+				go dc.loop()
 			}
-			f.log.WithFields(cue.Fields{"remote_addr": c.RemoteAddr().String()}).Info("debug connection opened")
-			atomic.AddUint32(&f.connCount, 1)
-			f.conns.Store(c.RemoteAddr().String(), c)
 		}
 	}(ln)
 	return nil
 }
 
-func (f *debugForwader) Shutdown(os.Signal) {
+func (f *debugForwader) Shutdown(sig os.Signal) {
 	if f == nil {
 		return
 	}
+	close(f.quit)
+	<-f.exited
 	if f.connLn != nil {
 		f.connLn.Close()
 	}
 
 	f.conns.Range(func(k, v interface{}) bool {
-		c := v.(net.Conn)
-		c.Close()
+		c := v.(*debugConn)
+		c.closeAndWait()
 		return true
 	})
 }
@@ -107,19 +139,54 @@ func (f *debugForwader) forward(data []byte) {
 		return
 	}
 	f.conns.Range(func(k, v interface{}) bool {
-		c := v.(net.Conn)
-		go func(k interface{}, c net.Conn, d []byte) {
-			_, err := c.Write(d)
-			if err != nil {
-				f.Lock()
-				if _, ok := f.conns.Load(k); ok {
-					f.log.WithFields(cue.Fields{"source": k}).Info("debug connection closed")
-					f.conns.Delete(k)
-					atomic.AddUint32(&f.connCount, ^uint32(0))
-				}
-				f.Unlock()
-			}
-		}(k, c, data)
+		c := v.(*debugConn)
+		select {
+		case c.msgs <- data:
+		default:
+			// TODO: log that debug conn can't keep up with the speed
+		}
 		return true
 	})
+}
+
+func (f *debugForwader) remove(k string) {
+	f.Lock()
+	if _, ok := f.conns.Load(k); ok {
+		f.log.WithFields(cue.Fields{"source": k}).Info("debug connection closed")
+		f.conns.Delete(k)
+		atomic.AddUint32(&f.connCount, ^uint32(0))
+	}
+	f.Unlock()
+}
+
+func (c *debugConn) Close() {
+	c.o.Do(func() { close(c.msgs) })
+}
+
+func (c *debugConn) closeAndWait() {
+	c.Close()
+	c.closeWait.Wait()
+}
+
+func (c *debugConn) loop() {
+	defer c.closeWait.Done()
+	defer c.Conn.Close()
+	defer c.forwarder.remove(c.RemoteAddr().String())
+	for {
+		data, ok := <-c.msgs
+		if !ok {
+			return
+		}
+		if len(data) == 0 {
+			continue
+		}
+		err := c.SetWriteDeadline(time.Now().Add(time.Second))
+		if err == nil {
+			_, err = c.Write(data)
+		}
+		if err != nil {
+			c.forwarder.log.WithFields(cue.Fields{"error": err}).Info("debug connection forwarding failed, terminate")
+			return
+		}
+	}
 }
